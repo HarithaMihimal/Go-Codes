7852b267c8bad01542d494dbd948524b165f35cf
HarithaMihimal <harithamihimal4@gmail.com>
2024-04-26 08:56:20 +0530

    Navigate Hello World

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import "net/http"
 
 func main() {
+	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+w.Write([]byte("Hello World!"))
+	})
 	http.ListenAndServe(":8080", nil)
 
 }
